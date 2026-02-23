220fc37c4c298336f5ef36a3ff055d2271011d4c
Hirzee <faruqhirzi698@gmail.com>
2026-02-23 12:00:22 +0700

    Menghitung keliling segitiga

---
+++ introduction4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int keliling, a, b, c;
+
+int main ()
+{
+    int keliling, a, b, c;
+
+    cout << "sisi segitiga 1:";
+    cin >> a;
+
+    cout << "sisi segitiga 2:";
+    cin >> b;
+
+    cout << "sisi segitiga 3:";
+    cin >> c;
+
+    //Penghitung
+    keliling = a + b + c;
+
+    //Hasil
+    cout << "Hasilnya Adalah:" << keliling << endl;
+}
